47a89304de8a3f6b4deef533a2d4382ca57698e7
Nicolas Bigaouette <nbigaouette@gmail.com>
2017-11-02 21:46:23 -0400

    Create links for files, not directories

--- install.sh
+++ install.sh
@@ -14,12 +14,25 @@ function run_cmd() {
 }
 
 
-for f in to_link/*; do
-    new_dot_file="${INSTALL_DIR}/.$(basename ${f})"
-    cmd="mkdir -p $(dirname ${new_dot_file})"
+pushd to_link
+
+# Normal files
+files_list=($(find * -type f -print))
+# Symbolic links
+files_list+=($(find * -type l -print))
+
+popd
+
+
+for f in ${files_list[*]}; do
+    new_dot_file="${INSTALL_DIR}/.${f}"
+    new_dir="$(dirname ${new_dot_file})"
+    # echo "f: ${f}   new_dot_file: ${new_dot_file}   new_dir: ${new_dir}"
+
+    cmd="mkdir -p ${new_dir}"
     run_cmd ${cmd}
 
-    cmd="ln -s ${SCRIPT_DIR}/${f} ${new_dot_file}"
+    cmd="ln -s ${SCRIPT_DIR}/to_link/${f} ${new_dot_file}"
     run_cmd ${cmd}
 done
 
