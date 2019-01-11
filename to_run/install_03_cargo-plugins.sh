fd77bce8f8767dddf0cf9e53940830f9148854cf
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-01-11 11:11:46 -0500

    Replace cargo-update install script with generic that installs multiple plugins

---
+++ to_run/install_03_cargo-plugins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+to_install=""
+
+function append_to_install() {
+    app_name="${1}"
+    # Optionnaly, if second argument is present, use it as command to test.
+    # Useful when binary is not named the same as application.
+    app_bin="${2:-${app_name}}"
+    # app_pkg="${3:-${app_name}}"
+    # if [[ -x "$(command -v ${app_bin})" ]]; then
+    #     echo "Warning: ${app_name} already installed."
+    # else
+    #     to_install="${to_install} ${app_pkg}"
+    # fi
+
+    if [[ -x "${HOME}/.cargo/bin/${app_bin}" ]]; then
+        echo "Warning: ${app_name} already installed."
+    else
+        echo "Adding ${app_name}"
+        to_install="${to_install} ${app_name}"
+    fi
+}
+
+append_to_install cargo-audit
+append_to_install cargo-edit cargo-add
+append_to_install cargo-license
+append_to_install cargo-outdated
+append_to_install cargo-tree
+append_to_install cargo-update cargo-install-update
+
+if [[ "${to_install}" == "" ]]; then
+    echo "Warning: Nothing to install."
+else
+    # Use cargo install directly
+    cmd="cargo install ${to_install}"
+    echo ${cmd}
+    ${cmd}
+fi
