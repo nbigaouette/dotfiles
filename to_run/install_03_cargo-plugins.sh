1547b4f7920f2785bf88f5947e78dafb18bc934b
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-01-11 19:43:26 -0500

    Cleanup

--- to_run/install_03_cargo-plugins.sh
+++ to_run/install_03_cargo-plugins.sh
@@ -7,13 +7,6 @@ function append_to_install() {
     # Optionnaly, if second argument is present, use it as command to test.
     # Useful when binary is not named the same as application.
     app_bin="${2:-${app_name}}"
-    # app_pkg="${3:-${app_name}}"
-    # if [[ -x "$(command -v ${app_bin})" ]]; then
-    #     echo "Warning: ${app_name} already installed."
-    # else
-    #     to_install="${to_install} ${app_pkg}"
-    # fi
-
     if [[ -x "${HOME}/.cargo/bin/${app_bin}" ]]; then
         echo "Warning: ${app_name} already installed."
     else
